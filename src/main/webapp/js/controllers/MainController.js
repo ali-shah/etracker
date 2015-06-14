4659723e4d151fa3278da06dd0c3c693f663cf5b
ali-shah <ali.shah.dr@gmail.com>
2015-06-15 10:59:02 +1200

    - updated the frontend controller

--- src/main/webapp/js/controllers/MainController.js
+++ src/main/webapp/js/controllers/MainController.js
@@ -1,35 +1,53 @@
 app.controller('MainController', function ($scope, Vehicle) {
-    $scope.process = function () {
-        //alert("Hellow world " + $scope.rego);
-        Vehicle.get({vehicleRego: $scope.rego}, function (data) {
-            $scope.vehicle = data;
-            createMarker(data);
-        });
-
-
+    $scope.map;
+    var markers = [];
+    function initialize() {
+        var centerPoint = new google.maps.LatLng(-36.848564, 174.765308);
         var mapOptions = {
             zoom: 15,
-//            center: new google.maps.LatLng(-36.848564, 174.765308),
-//            center: new google.maps.LatLng($scope.vehicle.latitude, $scope.vehicle.langitude),
+            center: centerPoint,
             mapTypeId: google.maps.MapTypeId.TERRAIN
-        }
-
+        };
         $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
-        $scope.map.setCenter(new google.maps.LatLng(-36.848564, 174.765308));
-//        $scope.map.panTo(new google.maps.LatLng(-36.848564, 174.765308));
-//        $scope.map.panBy(100,100);
-        $scope.markers = [];
+    }
+    function deleteMarkers() {
+        clearMarkers();
+        markers = [];
+    }
+    function clearMarkers() {
+        setAllMap(null);
+    }
+    // Sets the map on all markers in the array.
+    function setAllMap(map) {
+        for (var i = 0; i < markers.length; i++) {
+            markers[i].setMap(map);
+        }
+    }
 
+    $scope.process = function () {
+        var data = Vehicle.get({vehicleRego: $scope.rego}, function () {
+            $scope.vehicle = data;
+            if (typeof $scope.map === 'undefined') {
+                initialize();
+            }
+            if (typeof data.city === 'undefined') {
+                $('#myModal').modal('show');
+                $scope.map.setCenter(new google.maps.LatLng(-36.848564, 174.765308));
+            } else {
+                createMarker(data);
+                $scope.map.panTo(new google.maps.LatLng(data.latitude, data.langitude));
+                $scope.map.setZoom(15);
+            }
+        });
         var infoWindow = new google.maps.InfoWindow();
-
         var createMarker = function (info) {
-            var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+            deleteMarkers();
             var image = 'images/marker3.png';
             var marker = new google.maps.Marker({
                 map: $scope.map,
                 position: new google.maps.LatLng(info.latitude, info.langitude),
                 title: info.city,
-                icon: image /*iconBase + 'schools_maps.png'*/
+                icon: image
             });
             marker.content = '<div class="infoWindowContent"> <b>Registration</b>: ' + info.vehicleRego + '<br><b>Driver Name</b>: ' + info.driverName + '</div>';
 
@@ -38,14 +56,11 @@ app.controller('MainController', function ($scope, Vehicle) {
                 infoWindow.open($scope.map, marker);
             });
 
-            $scope.markers.push(marker);
-
+            markers.push(marker);
         }
-
         $scope.openInfoWindow = function (e, selectedMarker) {
             e.preventDefault();
             google.maps.event.trigger(selectedMarker, 'click');
         }
-
     };
 });
